ece0ad229786eb4d3bd3fcddc2342eb6f49eae2f
ardalan ebrahimi <ardalan.ebrahimi@gmail.com>
2024-10-30 21:41:55 +0100

    add background image to header and change titles

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'https://ardiland-dev.azurewebsites.net/api/products'
-  };
+  production: false,
+  apiUrl: 'https://localhost:44375/api/products',
+};
