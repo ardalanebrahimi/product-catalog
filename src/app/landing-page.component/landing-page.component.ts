10b9b333a5fc568cf2cd6a5419e0ebd208658f17
ardalan ebrahimi <ardalan.ebrahimi@gmail.com>
2024-10-30 23:59:29 +0100

    change featured products to read from api

--- src/app/landing-page.component/landing-page.component.ts
+++ src/app/landing-page.component/landing-page.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../service/product.service';
+import { Product } from '../model/product.model';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
-export class LandingPageComponent { }
+export class LandingPageComponent {
+  products?: Product[];
+  constructor(private productService: ProductService) {
+    this.loadProducts();
+  }
+
+  private loadProducts() {
+    this.productService.getProducts().subscribe((products) => {
+      this.products = products;
+    });
+  }
+}
