5c89951643d3bb43c924b40345cecec96e4cb5f0
ardalan ebrahimi <ardalan.ebrahimi@gmail.com>
2024-11-03 14:55:06 +0100

    fix the dev url

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44375/api/',
+  apiUrl: 'https://ardiland-dev.azurewebsites.net/api/',
   stripePublicKey:
     'pk_test_51QH0pdR7cEzPrCBOgdHvHUUdW2Wy55Sn8ItdCdKJnnQ1b0z76FGEbshrevqm7HXPPNnnYHW3OZRVqZRrU2kap6eh00xGQEE4QG',
 };
